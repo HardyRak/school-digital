c743a8bfd0b57d488e7753be9487779832260f72
HardyRak <119031361+HardyRak@users.noreply.github.com>
2024-09-18 23:00:00 +0300

    heheh

--- src/main/java/com/hard/cegAndranovelona/CegAndranovelonaApplication.java
+++ src/main/java/com/hard/cegAndranovelona/CegAndranovelonaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CegAndranovelonaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello hehhe");
 		SpringApplication.run(CegAndranovelonaApplication.class, args);
 	}
 
